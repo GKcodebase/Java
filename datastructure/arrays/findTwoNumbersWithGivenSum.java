c7e3808901761b653226bd0f9defec82f5e3503f
Gokul G.K <gokulgk1200@gmail.com>
2023-07-04 09:33:35 +0530

    Update findTwoNumbersWithGivenSum.java
    Solution with quicksort

--- datastructure/arrays/findTwoNumbersWithGivenSum.java
+++ datastructure/arrays/findTwoNumbersWithGivenSum.java
@@ -3,6 +3,8 @@ package datastructure.arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Arrays.sort;
+
 /**
  * The type Find two numbers with given sum.
  */
@@ -31,6 +33,39 @@ public class findTwoNumbersWithGivenSum {
     }
 
     /**
+     * Quick sort and find sum int [ ].
+     *
+     * @param arr the arr
+     * @param n   the n
+     * @return the int [ ]
+     */
+    public int[] quickSortAndFindSum(int[] arr , int n){
+        //Helper sort function that uses the Quicksort Algorithm
+        sort(arr,0,arr.length-1);
+
+        int Pointer1 = 0;    //Pointer 1 -> At Start
+        int Pointer2 = arr.length - 1;   //Pointer 2 -> At End
+        int[] result = new int[2];
+        int sum = 0;
+
+        while (Pointer1 != Pointer2) {
+
+            sum = arr[Pointer1] + arr[Pointer2];  //Calulate Sum of Pointer 1 and 2
+
+            if (sum < n)
+                Pointer1++;  //if sum is less than given value => Move Pointer 1 to Right
+            else if (sum > n)
+                Pointer2--;
+            else {
+                result[0] = arr[Pointer1];
+                result[1] = arr[Pointer2];
+                return result; // containing 2 number
+            }
+        }
+        return arr;
+    }
+
+    /**
      * Main.
      *
      * @param args the args
@@ -40,6 +75,13 @@ public class findTwoNumbersWithGivenSum {
         int[] ans =  new findTwoNumbersWithGivenSum().findNumbers(arr,27);
         System.out.print("Output :: ");
         for(int i:ans){
+            System.out.print(i+", ");
+        }
+
+
+        int[] ans2 =  new findTwoNumbersWithGivenSum().quickSortAndFindSum(arr,27);
+        System.out.print("\n"+"Solution with QuickSort :: ");
+        for(int i:ans2){
             System.out.print(i+", ");
         }
     }
