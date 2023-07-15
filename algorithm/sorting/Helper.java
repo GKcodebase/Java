57303ecef45a1f56709c93d93fd7b3f5c678b425
Gokul G.K <gokulgk1200@gmail.com>
2023-07-15 23:41:20 +0530

    Update Helper.java

--- algorithm/sorting/Helper.java
+++ algorithm/sorting/Helper.java
@@ -1,12 +1,30 @@
 package algorithm.sorting;
 
-import java.util.*;
+
+/**
+ * The type Helper.
+ */
 class Helper {
+    /**
+     * Print array.
+     *
+     * @param arr     the arr
+     * @param arrSize the arr size
+     */
     static void printArray(int[] arr, int arrSize) {
         for (int i = 0; i < arrSize; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
+
+    /**
+     * Find min int.
+     *
+     * @param arr   the arr
+     * @param start the start
+     * @param end   the end
+     * @return the int
+     */
     static int findMin(int[] arr, int start, int end) {
         if (end <= 0 || start < 0)
             return 0;
@@ -18,6 +36,15 @@ class Helper {
         }
         return minInd;
     }
+
+    /**
+     * Find max int.
+     *
+     * @param arr   the arr
+     * @param start the start
+     * @param end   the end
+     * @return the int
+     */
     int findMax(int[] arr, int start, int end) {
         if (end <= 0 || start < 0)
             return 0;
