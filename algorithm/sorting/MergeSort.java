1374361bea9bf393bb494bed2608bd17a4dc8fae
Gokul G.K <gokulgk1200@gmail.com>
2023-07-18 22:07:00 +0530

    Update MergeSort.java

--- algorithm/sorting/MergeSort.java
+++ algorithm/sorting/MergeSort.java
@@ -1,9 +1,38 @@
 package algorithm.sorting;
 
 /**
- * The type Merge.
+ * The type Merge sort.
  */
-class Merge {
+public class MergeSort {
+
+    /**
+     * The Obj.
+     */
+    static Helper obj = new Helper();
+
+    /**
+     * Merge sort.
+     *
+     * @param arr        the arr
+     * @param leftIndex  the left index
+     * @param rightIndex the right index
+     */
+    public static void mergeSort(int[] arr, int leftIndex, int rightIndex){
+        if (leftIndex < 0 || rightIndex < 0)
+            return;
+        if (rightIndex > leftIndex) {
+            // Equivalent to (leftIndex+rightIndex)/2, but avoids overflow
+            int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+            // Sorting the first and second halves of the array
+            mergeSort(arr, leftIndex, mid);
+            mergeSort(arr, mid + 1, rightIndex);
+
+            // Merging the array
+            merge(arr, leftIndex, mid, rightIndex);
+        }
+    }
+
     /**
      * Merge.
      *
@@ -13,9 +42,7 @@ class Merge {
      * @param right the right
      */
     public static void merge(int arr[], int left, int mid, int right) {
-        int i, j, k;
-
-        // Initialzing the sizes of the temporary arrays
+        int i,j,k;
         int sizeLeft = mid - left + 1;
         int sizeRight = right - mid;
 
@@ -23,16 +50,14 @@ class Merge {
         int leftArr[] = new int[sizeLeft];
         int rightArr[] = new int[sizeRight];
 
-        // Copying the given array into the temporary arrays
         for (i = 0; i < sizeLeft; i++)
             leftArr[i] = arr[left + i];
         for (j = 0; j < sizeRight; j++)
             rightArr[j] = arr[mid + 1 + j];
 
-        // Merging the temporary arrays back into the given array
-        i = 0; // Initial index of first subarray
-        j = 0; // Initial index of second subarray
-        k = left; // Initial index of the given array
+        i=0;
+        j=0;
+        k=left;
 
         // This is the main part of the algorithm
         // Iterate over both arrays and copy the element that is smaller to the
@@ -47,6 +72,7 @@ class Merge {
             }
             k++;
         }
+
 
         // Copying the remaining elements of leftArr[], if there are any
         while (i < sizeLeft) {
@@ -64,41 +90,16 @@ class Merge {
     }
 
     /**
-     * Merge sort.
-     *
-     * @param arr        the arr
-     * @param leftIndex  the left index
-     * @param rightIndex the right index
-     */
-    public static void mergeSort(int arr[], int leftIndex, int rightIndex) {
-        // Sanity checks
-        if (leftIndex < 0 || rightIndex < 0)
-            return;
-
-        if (rightIndex > leftIndex) {
-            // Equivalent to (leftIndex+rightIndex)/2, but avoids overflow
-            int mid = leftIndex + (rightIndex - leftIndex) / 2;
-
-            // Sorting the first and second halves of the array
-            mergeSort(arr, leftIndex, mid);
-            mergeSort(arr, mid + 1, rightIndex);
-
-            // Merging the array
-            merge(arr, leftIndex, mid, rightIndex);
-        }
-    }
-
-    /**
      * Main.
      *
      * @param args the args
      */
-    public static void main(String args[]) {
-        Helper obj = new Helper();
-        int arr[] = {5,4,1,0,5,95,4,-100,200,0};
-        int arrSize = 10;
-        obj.printArray(arr, arrSize);
-        mergeSort(arr, 0, arrSize - 1);
-        obj.printArray(arr, arrSize);
-    }
+    public static void main(String[] args){
+            int[] array = {2,5,1,7,9,45,0,12,69,-30,100,24,-9};
+            int arrSize=array.length;
+            obj.printArray(array, arrSize);
+            mergeSort(array,0,arrSize-1);
+            obj.printArray(array, arrSize);
+
+        }
 }
