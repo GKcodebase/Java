72f75595b3083027e7c3b47cc1798e84c9528a47
Gokul G.K <gokulgk1200@gmail.com>
2023-07-29 16:54:21 +0530

    Create Node.java

---
+++ algorithm/tree/bst/Node.java
@@ -0,0 +1,33 @@
+package algorithm.tree.bst;
+
+/**
+ * The type Node.
+ *
+ * @param <T> the type parameter
+ */
+public class Node<T> {
+    /**
+     * The Data.
+     */
+    T data;
+    /**
+     * The Left.
+     */
+    Node left;
+    /**
+     * The Right.
+     */
+    Node right;
+
+    /**
+     * Instantiates a new Node.
+     *
+     * @param data the data
+     */
+    public Node(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
