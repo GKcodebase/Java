f1a0d7674573faaa71a75dfccd2b94717ec12fb9
Gokul G.K <gokulgk1200@gmail.com>
2023-08-05 13:35:45 +0530

    Update GraphTranspose.java

--- algorithm/graph/GraphTranspose.java
+++ algorithm/graph/GraphTranspose.java
@@ -1,5 +1,7 @@
 package algorithm.graph;
 
+import java.util.LinkedList;
+
 /**
  * The type Graph transpose.
  */
@@ -12,9 +14,14 @@ public class GraphTranspose {
      */
     public static void getTranspose(Graph g) {
         int numOfVertices = g.getVertices(); //getVertices defined in Graph.java file
-
+        LinkedList<Integer> Llist[];
+        Llist = g.getAdj();
         Graph transposedGraph = new Graph(numOfVertices); //new graph to store the transpose
-
+        for (int v = 0; v < numOfVertices; v++) {
+            for (int i = 0; i < Llist[v].size(); i++) {
+                transposedGraph.addEdge(Llist[v].get(i), v);
+            }
+        }
         transposedGraph.printGraph(); //calling print function on the final transposed graph
     }
 
@@ -24,6 +31,16 @@ public class GraphTranspose {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-
+        Graph g1 = new Graph(5);
+        g1.addEdge(0, 1);
+        g1.addEdge(1, 2);
+        g1.addEdge(2, 3);
+        g1.addEdge(3, 0);
+        g1.addEdge(2, 4);
+        g1.addEdge(4, 2);
+        System.out.println("Given Graph: ");
+        g1.printGraph();
+        System.out.println("Transpose of given Graph: ");
+        getTranspose(g1);
     }
 }
