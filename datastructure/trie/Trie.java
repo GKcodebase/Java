837e2068ad8b982dbc7f10354e919b18cf6c11e7
Gokul G.K <gokulgk1200@gmail.com>
2023-08-14 23:02:10 +0530

    Update Trie.java
    Delete in Trie

--- datastructure/trie/Trie.java
+++ datastructure/trie/Trie.java
@@ -31,7 +31,7 @@ public class Trie {
     /**
      * Insert.
      *
-     * @param key   the key
+     * @param key the key
      */
 //Function to insert a key,value pair in the Trie
     public void insert(String key){
@@ -68,7 +68,7 @@ public class Trie {
         TrieNode currentNode = this.root;
         int index =0;
 
-        for(int level =0;level<=key.length();level++){
+        for(int level =0;level<key.length();level++){
             index = getIndex(key.charAt(level));
             if(currentNode.getChildren()[index]==null)
                 return false;
@@ -84,5 +84,82 @@ public class Trie {
      * @return the boolean
      */
 //Function to delete given key from Trie
-    public boolean delete(String key){ return false;}
+    public boolean delete(String key){
+        if(null==root || null == key)
+            return false;
+        return deleteHelper(key,root,key.length(),0);
+    }
+
+    /**
+     * Delete helper boolean.
+     *
+     * @param key         the key
+     * @param currentNode the current node
+     * @param length      the length
+     * @param level       the level
+     * @return the boolean
+     */
+    private boolean deleteHelper(String key, TrieNode currentNode, int length, int level) {
+        boolean deletedSelf = false;
+
+        if (currentNode == null) {
+            System.out.println("Key does not exist");
+            return deletedSelf;
+        }
+
+        //Base Case: If we have reached at the node which points to the alphabet at the end of the key.
+        if (level == length) {
+            //If there are no nodes ahead of this node in this path
+            //Then we can delete this node
+            if (hasNoChildren(currentNode)) {
+                currentNode = null;
+                deletedSelf = true;
+            }
+            //If there are nodes ahead of currentNode in this path
+            //Then we cannot delete currentNode. We simply unmark this as leaf
+            else {
+                currentNode.unMarkAsLeaf();
+                deletedSelf = false;
+            }
+        } else {
+            TrieNode childNode = currentNode.getChildren()[getIndex(key.charAt(level))];
+            boolean childDeleted = deleteHelper(key, childNode, length, level + 1);
+            if (childDeleted) {
+                //Making children pointer also null: since child is deleted
+                currentNode.getChildren()[getIndex(key.charAt(level))] = null;
+                //If currentNode is leaf node that means currntNode is part of another key
+                //and hence we can not delete this node and it's parent path nodes
+                if (currentNode.isEndWord()) {
+                    deletedSelf = false;
+                }
+                //If childNode is deleted but if currentNode has more children then currentNode must be part of another key.
+                //So, we cannot delete currenNode
+                else if (!hasNoChildren(currentNode)) {
+                    deletedSelf = false;
+                }
+                //Else we can delete currentNode
+                else {
+                    currentNode = null;
+                    deletedSelf = true;
+                }
+            } else {
+                deletedSelf = false;
+            }
+        }
+        return deletedSelf;
+    }
+
+    /**
+     * Has no children boolean.
+     *
+     * @param currentNode the current node
+     * @return the boolean
+     */
+    private boolean hasNoChildren(TrieNode currentNode) {
+        for (int i = 0; i < currentNode.getChildren().length; i++) {
+            if ((currentNode.getChildren()[i]) != null)
+                return false;
+        }
+        return true;
+    }
 }
