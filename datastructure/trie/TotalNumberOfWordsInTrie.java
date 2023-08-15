ca4920391d0b7eaa3d7793a7f5c206d991cf40a9
Gokul G.K <gokulgk1200@gmail.com>
2023-08-15 09:06:11 +0530

    Update TotalNumberOfWordsInTrie.java

--- datastructure/trie/TotalNumberOfWordsInTrie.java
+++ datastructure/trie/TotalNumberOfWordsInTrie.java
@@ -2,7 +2,16 @@ package datastructure.trie;
 
 import java.util.Arrays;
 
+/**
+ * The type Total number of words in trie.
+ */
 public class TotalNumberOfWordsInTrie {
+    /**
+     * Count word int.
+     *
+     * @param root the root
+     * @return the int
+     */
     public static int countWord(TrieNode root){
         int count =0;
         if(root.isEndWord())
@@ -14,6 +23,11 @@ public class TotalNumberOfWordsInTrie {
         return count;
     }
 
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
     public static void main(String args[]){
         // Input keys (use only 'a' through 'z' and lower case)
         String keys[] = {"the", "a", "there", "answer", "any",
