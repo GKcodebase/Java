04b185b39bca1444b1f1087143de0d86bde19c2f
Gokul G.K <gokulgk1200@gmail.com>
2023-08-21 08:34:08 +0530

    Create LargestNumber.java

---
+++ algorithm/greedyalgorithm/LargestNumber.java
@@ -0,0 +1,80 @@
+package algorithm.greedyalgorithm;
+
+/**
+ * The type Largest number.
+ */
+public class LargestNumber {
+    /**
+     * Find sum of digits int.
+     *
+     * @param num the num
+     * @return the int
+     */
+    public static int findSumOfDigits(int num) {
+
+        int sum = 0;
+        while (num != 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    /**
+     * Gets largest number.
+     *
+     * @param size the size
+     * @param sum  the sum
+     */
+    //Brute Force
+    public static void getLargestNumber(int size, int sum) {
+        int num = 0;
+        int startRange = (int) Math.pow(10, (size - 1));
+        int endRange = (int) Math.pow(10, size);
+        if (sum == 0) {
+            if (size == 1)
+                System.out.println("Largest number is " + 0);
+            else
+                System.out.println("Largest number is Not possible");
+            return;
+        }
+        // sumOfDigits is greater than the maximum possible sum.
+        if (sum > 9 * size) {
+            System.out.println("Largest number is Not possible");
+            return;
+        }
+        while (startRange < endRange) {
+            if (findSumOfDigits(startRange) == sum) {
+                if (num < startRange)
+                    num = startRange;
+            }
+            startRange++;
+        }
+        System.out.println("Largest number is " + num);
+    }
+
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        int sumOfDigits = 20;
+        int numberOfDigits = 3;
+
+        System.out.println("If sum of digits is 20 and number of digits is 3 then ");
+        getLargestNumber(numberOfDigits, sumOfDigits);
+        System.out.println();
+
+        //Example 2
+        sumOfDigits = 100;
+        numberOfDigits = 2;
+
+        System.out.println("If sum of digits is 100 and number of digits is 2 then ");
+        getLargestNumber(numberOfDigits, sumOfDigits);
+
+        System.out.println("If sum of digits is 5 and number of digits is 2 then ");
+        getLargestNumber(2, 5);
+    }
+}
