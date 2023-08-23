8fae3ba0510adb784f10546eee2f8bd0eeb862b1
Gokul G.K <gokulgk1200@gmail.com>
2023-08-23 08:23:15 +0530

    Update FibonacciNumber.java
    recurssion

--- algorithm/DynamicProgramming/FibonacciNumber.java
+++ algorithm/DynamicProgramming/FibonacciNumber.java
@@ -11,7 +11,15 @@ public class FibonacciNumber {
         return arr[n-1];
     }
 
+    public static int fib(int i) {
+        if (i <= 1)
+            return i;
+        return fib(i - 1) + fib(i - 2);
+    }
+
     public static void main(String[] args){
         System.out.println("Fibonacci number of 6 is "+getFibonacciUsingDynamicProgramming(6));
+        System.out.println("Fibonacci number of 5 is "+fib(5));
+
     }
 }
