20f72bf2528fb4612b39dd54b35c921142becaec
Gokul G.K <gokulgk1200@gmail.com>
2023-08-23 08:23:29 +0530

    Update FibonacciNumber.java

--- algorithm/DynamicProgramming/FibonacciNumber.java
+++ algorithm/DynamicProgramming/FibonacciNumber.java
@@ -1,7 +1,16 @@
 package algorithm.DynamicProgramming;
 
+/**
+ * The type Fibonacci number.
+ */
 public class FibonacciNumber {
 
+    /**
+     * Get fibonacci using dynamic programming int.
+     *
+     * @param n the n
+     * @return the int
+     */
     public static int getFibonacciUsingDynamicProgramming(int n){
         int[] arr = new int[n];
         arr[0]=1;arr[1]=1;
@@ -11,12 +20,23 @@ public class FibonacciNumber {
         return arr[n-1];
     }
 
+    /**
+     * Fib int.
+     *
+     * @param i the
+     * @return the int
+     */
     public static int fib(int i) {
         if (i <= 1)
             return i;
         return fib(i - 1) + fib(i - 2);
     }
 
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
     public static void main(String[] args){
         System.out.println("Fibonacci number of 6 is "+getFibonacciUsingDynamicProgramming(6));
         System.out.println("Fibonacci number of 5 is "+fib(5));
