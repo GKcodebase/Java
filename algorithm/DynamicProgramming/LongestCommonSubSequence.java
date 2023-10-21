0142d53e1468a8cf39828fb42026dedc0a6110ea
Gokul G.K <gokulgk1200@gmail.com>
2023-10-21 17:30:05 +0530

    Create LongestCommonSubSequence.java

---
+++ algorithm/DynamicProgramming/LongestCommonSubSequence.java
@@ -0,0 +1,47 @@
+package algorithm.DynamicProgramming;
+
+
+/**
+ * The type Longest common sub sequence.
+ */
+public class LongestCommonSubSequence {
+    /**
+     * Gets sub sequences.
+     *
+     * @param m the m
+     * @param n the n
+     * @param a the a
+     * @param b the b
+     * @return the sub sequences
+     */
+    public static int getSubSequences(int m, int n, String a, String b) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++)
+                dp[i][j] = -1;
+        }
+        return getSubSequencesUtil(m, n, a, b, dp);
+    }
+
+    /**
+     * Gets sub sequences util.
+     *
+     * @param m  the m
+     * @param n  the n
+     * @param a  the a
+     * @param b  the b
+     * @param dp the dp
+     * @return the sub sequences util
+     */
+    private static int getSubSequencesUtil(int m, int n, String a, String b, int[][] dp) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (dp[m][n] != -1)
+            return dp[m][n];
+        if (a.charAt(m - 1) == b.charAt(n - 1))
+            return dp[m][n] = 1 + getSubSequencesUtil(m - 1, n - 1, a, b, dp);
+        else {
+            return dp[m][n] = Math.max(getSubSequencesUtil(m - 1, n, a, b, dp), getSubSequencesUtil(m, n - 1, a, b, dp));
+        }
+    }
+}
