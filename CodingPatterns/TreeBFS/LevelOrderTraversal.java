8c0c3dda5c0e3adb41568473990d4eff516be708
Gokul G.K <gokulgk1200@gmail.com>
2024-07-18 16:54:34 +0530

    Update LevelOrderTraversal.java

--- CodingPatterns/TreeBFS/LevelOrderTraversal.java
+++ CodingPatterns/TreeBFS/LevelOrderTraversal.java
@@ -3,17 +3,46 @@ package CodingPatterns.TreeBFS;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * To implement a Binary Tree Level Order Traversal in Java,
+ * you can use a breadth-first search (BFS) approach,
+ * which utilizes a queue to traverse the tree level by level.
+ * Here's the step-by-step guide and the implementation:.
+ */
 public class LevelOrderTraversal {
 
+    /**
+     * The type Tree node.
+     */
     static class TreeNode {
+        /**
+         * The Value.
+         */
         int value;
+        /**
+         * The Left.
+         */
         TreeNode left;
+        /**
+         * The Right.
+         */
         TreeNode right;
 
+        /**
+         * Instantiates a new Tree node.
+         *
+         * @param value the value
+         */
         TreeNode(int value) {
             this.value = value;
         }
     }
+
+    /**
+     * Level order traversal.
+     *
+     * @param root the root
+     */
     public static void levelOrderTraversal(TreeNode root) {
         if (root == null) {
             return;
@@ -39,6 +68,11 @@ public class LevelOrderTraversal {
         }
     }
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
