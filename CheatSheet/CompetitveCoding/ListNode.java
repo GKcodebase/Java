5f7d9471f59116f7d3af200636b8717d4e28f3fd
Gokul G.K <gokulgk1200@gmail.com>
2024-12-13 18:22:16 -0600

    Create ListNode.java

---
+++ CheatSheet/CompetitveCoding/ListNode.java
@@ -0,0 +1,24 @@
+package CheatSheet.CompetitveCoding;
+
+/**
+ * The List node.
+ */
+public class ListNode {
+    /**
+     * The Val.
+     */
+    int val;
+    /**
+     * The Next.
+     */
+    ListNode next;
+
+    /**
+     * Instantiates a new List node.
+     *
+     * @param x the x
+     */
+    ListNode(int x) {
+        val = x;
+    }
+}
